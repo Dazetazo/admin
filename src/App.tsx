28cab5b62fa2acbb159fcabc124da02dbc63b954
Dazetazo <dazet@outlook.cl>
2025-06-30 13:42:21 -0400

    [+] Edit and Create Post

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Admin, Resource, ShowGuesser } from "react-admin";
 import { dataProvider } from './dataProvider';
 import { UserList } from "./user";
-import { PostList } from "./posts";
+import { PostList, PostEdit } from "./posts";
 
 //Pagina principal del admin
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="posts" list={PostList} />
+    <Resource name="posts" list={PostList} edit={PostEdit} /> {/* PostEdit permite editar los posts */}
     <Resource name="users" list={UserList} show={ShowGuesser}  /> {/* ShowGuesser muestra los detalles de un usuario al hacer click en su nombre */}
   </Admin>
 );
--- src/posts.tsx
+++ src/posts.tsx
@@ -1,4 +1,16 @@
-import { List, DataTable, ReferenceField, EditButton } from "react-admin";
+import {
+    List,
+    DataTable,
+    ReferenceField,
+    EditButton,
+    Edit,
+    SimpleForm,
+    ReferenceInput,
+    TextInput,
+    Create,
+} from "react-admin";
+
+
 
 // Componente para mostrar el listado de publicaciones
 
@@ -7,7 +19,7 @@ export const PostList = () => (
         <DataTable rowClick={false}>
             <DataTable.Col source="id" />
             <DataTable.Col source="userId">
-                <ReferenceField source="userId" reference="users" />
+                <ReferenceField source="userId" reference="users" link="show" />
             </DataTable.Col>
             <DataTable.Col source="title" />
             <DataTable.Col>
@@ -15,4 +27,29 @@ export const PostList = () => (
             </DataTable.Col>
         </DataTable>
     </List>
+);
+
+// Componente para editar una publicación
+
+export const PostEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <TextInput source="id" InputProps={{ disabled: true }} />
+            <ReferenceInput source="userId" reference="users" link="show" />
+            <TextInput source="title" />
+            <TextInput source="body" multiline rows={5} />
+        </SimpleForm>
+    </Edit>
+);
+
+// Componente para crear una nueva publicación
+
+export const PostCreate = () => (
+  <Create>
+    <SimpleForm>
+      <ReferenceInput source="userId" reference="users" />
+      <TextInput source="title" />
+      <TextInput source="body" multiline rows={5} />
+    </SimpleForm>
+  </Create>
 );
