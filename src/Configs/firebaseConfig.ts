c13768a7078fe30f9264eef3d8d1d3a95da002cf
Dazetazo <dazet@outlook.cl>
2025-06-30 17:29:21 -0400

    [+] .gitignore

--- src/Configs/firebaseConfig.ts
+++ src/Configs/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDAotgPbRuJT-64glrOaKUNuuCT3HW9Mos",
-  authDomain: "admin-2180b.firebaseapp.com",
-  projectId: "admin-2180b",
-  storageBucket: "admin-2180b.firebasestorage.app",
-  messagingSenderId: "1059983812310",
-  appId: "1:1059983812310:web:5ee3c56fda845ce802cb77",
-  measurementId: "G-2XEFS0X971"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
