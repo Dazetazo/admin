132b3ef82a1d235a6f1fde56638138410bc8a3b8
Dazetazo <dazet@outlook.cl>
2025-06-30 14:12:46 -0400

    [+] Update 0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { dataProvider } from './dataProvider';
 import { UserList } from "./user";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { Dashboard } from "./dashboard";
+import { authProvider } from "./authProvider";
 
 //Icons
 import PostIcon from "@mui/icons-material/Book";
@@ -12,7 +13,7 @@ import UserIcon from "@mui/icons-material/Group";
 //Pagina principal del admin
 
 export const App = () => (
-  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard} authProvider={authProvider}>
     {/* El componente Resource es el que define los recursos que se van a mostrar en la aplicación */}
     {/*Posts es el nombre del recurso, list es el componente que muestra el listado de publicaciones, edit es el componente que permite editar una publicación, create es el componente que permite crear una nueva publicación, icon es el icono que se muestra en la barra lateral*/}
     <Resource
